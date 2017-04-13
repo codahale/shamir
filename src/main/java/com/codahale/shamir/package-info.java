e317d1abad132f9f343803bd35d6dea181cf698b
Coda Hale <coda.hale@gmail.com>
2017-04-13 10:55:36 -0700

    Added package docs.

---
+++ src/main/java/com/codahale/shamir/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains an implementation of Shamir's Secret Sharing algorithm
+ * over {@code GF(256)}.
+ *
+ * @see com.codahale.shamir.SecretSharing
+ */
+package com.codahale.shamir;
