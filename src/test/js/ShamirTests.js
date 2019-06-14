430224e5935cfac87089c262ad36b34da0993e99
Simon Massey <simon@simonmassey.org>
2019-06-14 19:06:32 +0100

    mul

--- src/test/js/ShamirTests.js
+++ src/test/js/ShamirTests.js
@@ -5,17 +5,25 @@ const GF256 = require('../../main/js/GF256.js');
 test('GF256Tests add', function (t) {
     t.plan(1);
 
-    console.log(GF256);
-
     t.equal( GF256.add(100, 30), 122 );
 });
 
 test('GF256Tests sub', function (t) {
     t.plan(1);
 
-    console.log(GF256);
-
     t.equal( GF256.sub(100, 30), 122 );
+});
+
+test('GF256Tests mult', function (t) {
+    t.plan(4);
+
+
+    t.equal( GF256.mul(90, 21), 254 );
+    t.equal( GF256.mul(133, 5), 167 );
+
+    t.equal( GF256.mul(0, 21), 0 );
+    t.equal( GF256.mul(parseInt("0xb6"), parseInt("0x53")), parseInt("0x36") );
+
 });
 
 
