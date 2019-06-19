5a58b985218e7bc275fff0ab1d2eab9379916a3e
Simon Massey <simon@simonmassey.org>
2019-06-19 07:41:14 +0100

    round trips an anglo saxon Rune Poem

--- src/test/js/SchemeTests.js
+++ src/test/js/SchemeTests.js
@@ -20,20 +20,46 @@ const join = require('../../main/js/Scheme.js').join;
 
 const randomBytes = require('tweetnacl').randomBytes;
 
+// https://codereview.stackexchange.com/a/3589/75693
+function bytesToSring(bytes) {
+    var chars = [];
+    for(var i = 0, n = bytes.length; i < n;) {
+        chars.push(((bytes[i++] & 0xff) << 8) | (bytes[i++] & 0xff));
+    }
+    return String.fromCharCode.apply(null, chars);
+}
+
+// https://codereview.stackexchange.com/a/3589/75693
+function stringToBytes(str) {
+    var bytes = [];
+    for(var i = 0, n = str.length; i < n; i++) {
+        var char = str.charCodeAt(i);
+        bytes.push(char >>> 8, char & 0xFF);
+    }
+    return bytes;
+}
+
 test('SchemeTests roundtrip', function (t) {
     const parts = 3;
     const quorum = 2;
-    const secret = [];
-    secret[0] = 1;
-    secret[1] = 2;
-    // secret[2] = 3;
+
+    // http://kermitproject.org/utf8.html
+    // From the Anglo-Saxon Rune Poem (Rune version)    
+    const secretUtf8 = `ᚠᛇᚻ᛫ᛒᛦᚦ᛫ᚠᚱᚩᚠᚢᚱ᛫ᚠᛁᚱᚪ᛫ᚷᛖᚻᚹᛦᛚᚳᚢᛗ
+ᛋᚳᛖᚪᛚ᛫ᚦᛖᚪᚻ᛫ᛗᚪᚾᚾᚪ᛫ᚷᛖᚻᚹᛦᛚᚳ᛫ᛗᛁᚳᛚᚢᚾ᛫ᚻᛦᛏ᛫ᛞᚫᛚᚪᚾ
+ᚷᛁᚠ᛫ᚻᛖ᛫ᚹᛁᛚᛖ᛫ᚠᚩᚱ᛫ᛞᚱᛁᚻᛏᚾᛖ᛫ᛞᚩᛗᛖᛋ᛫ᚻᛚᛇᛏᚪᚾ᛬`;
+
+    var secret = stringToBytes(secretUtf8);
+
     const splits = split(randomBytes, parts, quorum, secret);
     t.equal(Object.keys(splits).length, parts);
-    //console.log(splits);
     const joined = join(splits);
     t.equal( joined[0], secret[0] );
     t.equal( joined[1], secret[1] );
-    // t.equal( joined[2], secret[2] );
+    const joinedUtf8 = bytesToSring(joined);
+    t.equal( secretUtf8, joinedUtf8 );
+    // console.log("The secret is: "+secretUtf8);
+    // console.log("The decoded secret is: "+joinedUtf8);
     t.end();
 });
 
