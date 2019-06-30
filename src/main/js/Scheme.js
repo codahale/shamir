8269caf77d76b6403c473fbcf9f346fdced7f63e
Simon Massey <simon@simonmassey.org>
2019-06-30 20:28:16 +0100

    fixed eslint issues

--- src/main/js/Scheme.js
+++ src/main/js/Scheme.js
@@ -19,22 +19,26 @@ const GF256 = require('./GF256.js');
 /**
  * Splits the given secret into {@code n} parts, of which any {@code k} or more can be combined to
  * recover the original secret.
- * @param  {function int -> Uint8Array} randomBytes Takes a length and returns a random Uint8Array of that length
+ * @param  {function int -> Uint8Array} randomBytes Takes a length and returns a random
+ * Uint8Array of that length
  * @param  {Number} n the number of parts to produce (must be {@code >1})
  * @param  {Number} k the threshold of joinable parts (must be {@code <= n})
  * @param  {array[Uint8Array]} secret The secret to split as an array of bytes
- * @return {Object.<string, Uint8Array>} an map of {@code n} parts that are arrays of bytes of the secret length
+ * @return {Object.<string, Uint8Array>} an map of {@code n} parts that are arrays of bytes of the
+ * secret length
  */
-exports.split = function(randomBytes, n, k, secret) {
-  if (k <= 1) throw 'K must be > 1';
-  if (n < k) throw 'N must be >= K';
-  if (n > 255) throw 'N must be <= 255';
+function split(randomBytes, n, k, secret) {
+  if (k <= 1) throw new Error('K must be > 1');
+  if (n < k) throw new Error('N must be >= K');
+  if (n > 255) throw new Error('N must be <= 255');
 
   const values = new Array(n)
     .fill(0)
     .map(() => new Uint8Array(secret.length).fill(0));
+  // eslint-disable-next-line no-plusplus
   for (let i = 0; i < secret.length; i++) {
     const p = GF256.generate(randomBytes, k - 1, secret[i]);
+    // eslint-disable-next-line no-plusplus
     for (let x = 1; x <= n; x++) {
       values[x - 1][i] = GF256.eval(p, x);
     }
@@ -42,13 +46,16 @@ exports.split = function(randomBytes, n, k, secret) {
 
   const parts = {};
 
+  // eslint-disable-next-line no-plusplus
   for (let i = 0; i < values.length; i++) {
     const part = `${i + 1}`;
     parts[part] = values[i];
   }
 
   return parts;
-};
+}
+
+exports.split = split;
 
 /**
  * Joins the given parts to recover the original secret.
@@ -57,23 +64,27 @@ exports.split = function(randomBytes, n, k, secret) {
  * original secret. If the parts are incorrect, or are under the threshold value used to split the
  * secret, a random value will be returned.
  *
- * @param {Object.<string, Uint8Array>} parts an map of {@code n} parts that are arrays of bytes of the secret length
+ * @param {Object.<string, Uint8Array>} parts an map of {@code n} parts that are arrays of bytes
+ * of the secret length
  * @return {Uint8Array} the original secret
  *
  */
-exports.join = function(parts) {
-  if (Object.keys(parts).length == 0) throw 'No parts provided';
+function join(parts) {
+  if (Object.keys(parts).length === 0) throw new Error('No parts provided');
   const lengths = Object.values(parts).map(x => x.length);
   const max = Math.max.apply(null, lengths);
   const min = Math.min.apply(null, lengths);
-  if (max != min)
-    throw `Varying lengths of part values. Min ${min}, Max ${max}`;
+  if (max !== min) {
+    throw new Error(`Varying lengths of part values. Min ${min}, Max ${max}`);
+  }
   const secret = new Uint8Array(max);
+  // eslint-disable-next-line no-plusplus
   for (let i = 0; i < secret.length; i++) {
     const keys = Object.keys(parts);
     const points = new Array(keys.length)
       .fill(0)
       .map(() => new Uint8Array(2).fill(0));
+    // eslint-disable-next-line no-plusplus
     for (let j = 0; j < keys.length; j++) {
       const key = keys[j];
       const k = Number(key);
@@ -84,4 +95,6 @@ exports.join = function(parts) {
   }
 
   return secret;
-};
+}
+
+exports.join = join;
